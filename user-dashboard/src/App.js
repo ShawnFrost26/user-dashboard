406e4e9a004991494234f8796babccbaed8b0aa4
ShawnFrost26 <sb262202@gmail.com>
2024-04-20 19:10:30 +0530

    populated users data in a table

--- user-dashboard/src/App.js
+++ user-dashboard/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import UserList from "./components/UserList";
 
 function App() {
-
   return (
     <div className="App">
+      <h1>User Dashboard</h1>
+      <UserList />
     </div>
   );
 }
---
+++ user-dashboard/src/components/UserList.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { getUsers } from './FetchData';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers()
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Username</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Company</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map(user => (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.company.name}</TableCell>
+              <TableCell>
+                <Button>Edit</Button>
+                <Button>Delete</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default UserList;
