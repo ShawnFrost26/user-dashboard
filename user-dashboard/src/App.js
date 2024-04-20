34c53aa03f62351f759c4baa5d7bf4f4a9180de9
ShawnFrost26 <sb262202@gmail.com>
2024-04-20 18:51:27 +0530

    Moved to FetchData component

--- user-dashboard/src/App.js
+++ user-dashboard/src/App.js
@@ -1,18 +1,7 @@
 import './App.css';
 
 function App() {
-  const API_URL = "https://jsonplaceholder.typicode.com/users";
 
- const getUsers = async () => {
-  const response = await fetch(API_URL);
-  if (!response.ok) {
-    throw new Error("Failed to fetch users");
-  }
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-getUsers();
   return (
     <div className="App">
     </div>
---
+++ user-dashboard/src/components/FetchData.jsx
@@ -0,0 +1,12 @@
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export const getUsers = async () => {
+  const response = await fetch(API_URL);
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+getUsers();
